563a82dee26480484b834ed0866248dd0d016e36
Mutalibb <temitopking@icloud.com>
2025-02-23 08:05:47 -0700

    game route added

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-link to="/"></router-link>
+  <div>
+    <div class="welcometext">Mutalibb</div>
+  </div>
+</template>
+
+<style>
+.welcometext {
+  background: linear-gradient(45deg, white, blue);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 3rem;
+  font-weight: bold;
+  border-radius: 0.5rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.9);
+  margin-left: 1rem;
+  text-align: center;
+  width: 80%;
+  margin: 0 auto;
+}
+.welcometext:hover {
+  background: linear-gradient(45deg, black, blue);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,15 +1,89 @@
+<script setup>
+import TopHeader from '@/components/TopHeader.vue'
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an game page</h1>
+  <div class="container">
+    <TopHeader />
+    <div class="cover">
+      <router-link to="/games">
+        <div class="smallbox">
+          <div class="smallboxtext"></div>
+        </div>
+      </router-link>
+      <router-link to="/games">
+        <div class="smallbox">
+          <div class="smallboxtext"></div>
+        </div>
+      </router-link>
+      <router-link to="/games">
+        <div class="smallbox">
+          <div class="smallboxtext"></div>
+        </div>
+      </router-link>
+      <router-link to="/games">
+        <div class="smallbox">
+          <div class="smallboxtext"></div>
+        </div>
+      </router-link>
+      <router-link to="/games">
+        <div class="smallbox">
+          <div class="smallboxtext"></div>
+        </div>
+      </router-link>
+      <router-link to="/games">
+        <div class="smallbox">
+          <div class="smallboxtext"></div>
+        </div>
+      </router-link>
+      <router-link to="/games">
+        <div class="smallbox">
+          <div class="smallboxtext"></div>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.container {
+  justify-content: center;
+  align-items: center;
+  height: auto;
+  background-color: rgba(0, 0, 0, 0.9);
+}
+.cover {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  height: auto;
+  padding: 1rem;
+}
+.smallbox {
+  width: 22rem;
+  padding: 10px;
+  height: 16.5rem;
+  background-color: rgb(0, 0, 0);
+  margin: 1rem;
+  margin-top: 0;
+  align-self: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 1px 1px 1px 1px white;
+}
+
+.smallboxtext {
+  background: linear-gradient(45deg, black, blue);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 3rem;
+  font-weight: bold;
+  border-radius: 0.5rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  margin-left: 1rem;
+  text-align: center;
 }
 </style>
